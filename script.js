cf0cc7f274d70a3e7e788307ca64a0e89b08be92
dongcheon1020 <dongcheon1020@gmail.com>
2024-02-12 15:56:32 +0900

    feat: javascript기능 추가, html 태그 추가, css 스타일 레이아웃 수정

--- script.js
+++ script.js
@@ -1,69 +1,78 @@
-// 랜덤번호 지정
-// 유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
-// 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다
-// 랜덤번호가 < 유저번호 down
-// 랜덤번호가 > 유저번호 up
-// reset 버튼을 누르면 게임이 리셋
-// 5번의 기회를 다 쓰면 게임이 끝난다.
-// 유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
-// 유저가 이미 입력한 숫자를 또 입력하면, 알려준다 , 기회를 깢지 않음
+let computerNum = 0;
+let playBt = document.querySelector(".play-bt");
+let userInput = document.getElementById("user-input");
+let resultArea = document.querySelector(".result-area-text");
+let resetBt = document.querySelector(".reset-bt");
+let chanceArea = document.querySelector(".chance-area");
+let history = [];
+let inputMessage = document.querySelector(".input-messege");
+let chanceAreaNum = 5;
+let gameOver = false;
 
-// let computerNum = 0;
-// let playBt = document.getElementById("play-bt");
-// let userInput = document.getElementById("user-input");
-// let resultArea = document.getElementById("result-area");
-// let resetBt = document.getElementById("reset-bt");
-// let chanceArea = document.getElementById("chance-area");
-// let chances = 5;
-// let gameOver = false;
-// let history = [];
+playBt.addEventListener("click", play);
+resetBt.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
+function pickRandomNum() {
+  computerNum = Math.floor(Math.random() * 100 + 1);
+}
+function play() {
+  let userValue = userInput.value;
+  inputMessage.textContent = "";
 
-// playBt.addEventListener("click", play);
-// resetBt.addEventListener("click", reset);
-// userInput.addEventListener("focus", function () {
-//   userInput.value = "";
-// });
+  if (userValue > 100 || userValue <= 0) {
+    inputMessage.textContent = "1 부터 100 사이의 숫자를 입력해주세요.";
+    return;
+  }
+  if (history.includes(userValue)) {
+    inputMessage.textContent = "이미 입력한 숫자 입니다.";
+    return;
+  } else {
+    history.push(userValue);
+    chanceAreaNum -= 1;
+    chanceArea.textContent = `남은 기회: ${chanceAreaNum}번`;
+  }
 
-// function pickRandomNum() {
-//   computerNum = Math.floor(Math.random() * 100 + 1);
-//   console.log("정답", computerNum);
-// }
-// function play() {
-//   let userValue = userInput.value;
+  if (userValue < computerNum) {
+    resultArea.textContent = "Up!";
+    resultArea.style = "color:#F7361B";
+  } else if (userValue > computerNum) {
+    resultArea.textContent = "Down!";
+    resultArea.style = "color:#1B66F7";
+  } else {
+    resultArea.textContent = "정답!";
+    resultArea.style = "color:#1BD939";
+    gameOver = true;
+  }
+  if (chanceAreaNum <= 0) {
+    gameOver = true;
+  }
 
-//   if (userValue < 1 || userValue > 100) {
-//     resultArea.textContent = "1과 100사이 숫자를 입력해 주세요";
-//     return;
-//   }
-//   if (history.includes(userValue)) {
-//     resultArea.textContent = "이미 입력한숫자 입니다.";
-//     return;
-//   }
-
-//   chances--;
-//   chanceArea.textContent = `남은기회: ${chances}번`;
-//   console.log(chances);
-//   if (userValue < computerNum) {
-//     resultArea.textContent = "Up!";
-//   } else if (userValue > computerNum) {
-//     resultArea.textContent = "Down!";
-//   } else {
-//     resultArea.textContent = "정답!";
-//     gameOver=true
-//   }
-//   history.push(userValue);
-//   console.log(history);
-
-//   if (chances <= 0) {
-//     gameOver = true;
-//   }
-//   if (gameOver == true) {
-//     playBt.disabled = true;
-//   }
-// }
-// function reset() {
-//   userInput.value = "";
-//   pickRandomNum();
-//   resultArea.textContent = "결과";
-// }
-// pickRandomNum();
+  if (gameOver) {
+    playBt.disabled = true;
+    userInput.disabled = true;
+    playBt.style = "display:none";
+    resetBt.style = "border-radius:8px";
+    resultArea.textContent = `게임 오버`;
+    resultArea.style = "color:#F7361B";
+    inputMessage.textContent = `정답: ${computerNum}`;
+    return;
+  }
+}
+function reset() {
+  pickRandomNum();
+  userInput.value = "";
+  resultArea.textContent = "시작";
+  resultArea.style = "color:#000";
+  inputMessage.textContent = "";
+  chanceAreaNum = 5;
+  chanceArea.textContent = `남은 기회: ${chanceAreaNum}번`;
+  history.splice(0, history.length);
+  playBt.disabled = false;
+  userInput.disabled = false;
+  gameOver = false;
+  playBt.style = "display:block";
+  resetBt.style = "border-radius:8px 0 0 8px";
+}
+pickRandomNum();
